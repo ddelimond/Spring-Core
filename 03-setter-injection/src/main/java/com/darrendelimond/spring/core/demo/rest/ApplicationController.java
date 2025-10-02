c42063e2b79e318448eb38c86fb2bcabb287f129
Darren Delimond <darrendelimond@gmail.com>
2025-10-02 14:30:03 -0400

    =performed setter injection of my bean / compenet, instead of constructor injection

---
+++ 03-setter-injection/src/main/java/com/darrendelimond/spring/core/demo/rest/ApplicationController.java
@@ -0,0 +1,32 @@
+package com.darrendelimond.spring.core.demo.rest;
+
+
+import com.darrendelimond.util.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApplicationController {
+
+
+    private Coach myCoach;
+
+    @Autowired
+    public void setCoach(Coach theCoach){
+
+        myCoach = theCoach;
+    }
+
+    @GetMapping("/")
+    public String greeting(){
+        return "Welcome to my site";
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout(){
+
+        return myCoach.getDailyWorkout();
+    }
+
+}
